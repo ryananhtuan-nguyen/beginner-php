cfea8e587a0595959438332e304436105ec86b64
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-17 15:20:18 +1300

    api call

--- 06_functions.php
+++ 06_functions.php
@@ -16,7 +16,18 @@ function sum($n1, $n2){
 echo sum(3,5);
 
 
+echo '<h1> Hello World </h1>';
 
+$response = file_get_contents('https://jsonplaceholder.typicode.com/posts/1');
+var_dump($response);
+$response = json_decode($response);
 
+foreach($response as $index => $content){
+  if($index == 'body') echo "<h2>$index</h2><br>" . $content;
+  else{
+  echo 
+  "<h2>$index</h2>$content<br>";
+  }
+}
 
 ?>
